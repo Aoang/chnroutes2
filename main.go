908ce05630921f509ccf251032731f09927168ce
Aoang <aoang@x2oe.com>
2022-12-01 02:43:06 +0000

    update ci

--- main.go
+++ main.go
@@ -10,34 +10,52 @@ import (
 )
 
 func main() {
+	cleanF()
+
 	for _, v := range getProjectFilename() {
 		genMD5SumFile(v)
 	}
 }
 
+func cleanF() {
+	bts, err := os.ReadFile("chnroutes2/chnroutes.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	arr := bytes.Split(bts, []byte("\n"))
+	buf := make([][]byte, 0, len(arr))
+
+	for i := range arr {
+		if len(arr[i]) > 0 && arr[i][0] == '#' {
+			continue
+		}
+		buf = append(buf, arr[i])
+	}
+
+	if err = os.WriteFile("chnroutes2/chnroutes.txt", bytes.Join(buf, []byte("\n")), 0666); err != nil {
+		log.Fatalln(err)
+	}
+}
+
 func genMD5SumFile(filename string) {
-	originData, err := os.ReadFile(filename)
+	originData, err := os.ReadFile("chnroutes2/" + filename)
 	if err != nil {
 		log.Fatalf("open %s err: %s\n", filename, err)
 	}
 
-	md5Data, _ := os.ReadFile(filename + ".md5sum.txt")
 	h := md5.Sum(originData)
-
-	if bytes.Equal(h[:], md5Data) {
-		return
-	}
 
 	dst := make([]byte, hex.EncodedLen(len(h)))
 	hex.Encode(dst, h[:])
 
-	if err = os.WriteFile(filename+".md5sum.txt", dst, 0666); err != nil {
+	if err = os.WriteFile("chnroutes2/"+filename+".md5sum.txt", dst, 0666); err != nil {
 		log.Fatalf("write file %s.md5sum.txt err: %s\n", filename, err)
 	}
 }
 
 func getProjectFilename() []string {
-	dirEntry, err := os.ReadDir(".")
+	dirEntry, err := os.ReadDir("chnroutes2")
 	if err != nil {
 		panic(err)
 	}
